5c37cacb1acf65535ae9c00b9c8ad3b30b5b154f
Tarek Chaalan <tchaalan23@icloud.com>
2024-04-22 02:24:51 -0700

    Minor Changes

--- screens/Workouts.tsx
+++ screens/Workouts.tsx
@@ -156,7 +156,7 @@ const styles = StyleSheet.create({
   headerTextContainer: {},
   ItemContainer: {},
   pageTitle: {
-    fontSize: 20,
+    fontSize: 36,
     marginTop: "6%",
     alignSelf: "center",
     fontFamily: "SFProRounded-Heavy",
@@ -168,7 +168,7 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     fontFamily: "SFProText-Light",
     color: "#fff",
-    marginBottom: "10%",
+    marginBottom: "5%",
   },
   boldText: {
     fontFamily: "SFProText-Heavy",
