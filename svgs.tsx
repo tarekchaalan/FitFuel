5aaaa1ee64108d7524fe851e386df711d8671c0f
Tarek Chaalan <tchaalan23@icloud.com>
2024-05-02 04:27:35 -0700

    Modified XButton

--- svgs.tsx
+++ svgs.tsx
@@ -31,10 +31,10 @@ export const DownArrow = () => (
 );
 
 export const XButton = () => (
-  <Svg height="15" width="15" viewBox="0 0 200 200">
+  <Svg height="18" width="18" viewBox="0 0 200 200">
     <Path
       d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"
-      fill="#fff"
+      fill="#aaa"
     />
   </Svg>
 );
