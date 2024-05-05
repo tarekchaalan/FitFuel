b63e472cbd161893b0fafd9b6715d32126128d46
Tarek Chaalan <tchaalan23@icloud.com>
2024-05-04 18:09:52 -0700

    deployment to app store changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { registerRootComponent } from "expo";
+import { AppRegistry } from "react-native";
 import App from "./App";
 
-registerRootComponent(App);
+AppRegistry.registerComponent("FitFuel", () => App);
